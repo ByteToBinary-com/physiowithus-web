35f9d179a067c5193d3221c4cc6e4c6ae70902d6
vaibhav <vaibhav@upbytes.in>
2026-03-07 13:54:10 +0530

    logo display

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,8 +24,8 @@ const Header = () => {
           <Image
             src="/physiowithus.png"
             alt="PhysioWithUs Logo"
-            width={36}
-            height={36}
+            width={100}
+            height={100}
             className="rounded-lg"
           />
           <span className="text-xl font-bold text-foreground">
